44d680b41866dbd6bfe92c4f2862c455aa907ed7
Kaio <luizkaio9815@gmail.com>
2021-10-12 13:13:43 -0300

    Cria o arquivo da atividade 1 do dia 11/10/2021

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Mensagem from './Mensagem';
+import './App.css';
+
+function App() {
+return (
+  <div className="App">
+      <Mensagem />
+    </div>
+  );
+}
+export default App;
---
+++ src/Mensagem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class Mensagem extends Component {
+    render(){
+        return(
+            <div>
+                <h1> Machado de Assis</h1>
+                <p>Há pessoas que choram por saber que as rosas têm espinho. Há outras que sorriem por saber que os espinhos têm rosas!</p>
+            </div>
+        )
+    }
+}
+export default Mensagem;
+
+
+// export const Mensagem = () => {
+
+//     <div>
+//         <p>Há pessoas que choram por saber que as rosas têm espinho. Há outras que sorriem por saber que os espinhos têm rosas!</p>
+//         <p> - Machado de Assis</p>
+//     </div>
+
+// };
